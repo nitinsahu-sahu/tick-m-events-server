4340bef74db003e0424690522c17525f9defca67
nitinsahu-sahu <nitinsahu911111@gmail.com>
2025-08-30 19:50:00 +0530

    Update Card

--- models/User.js
+++ models/User.js
@@ -185,14 +185,8 @@ const userSchema = new Schema({
     resetCodeExpires: Date
 }, { timestamps: true })
 
-// Hash password before saving
-userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) {
-        return next();
-    }
-    this.password = await bcrypt.hash(this.password, 10);
-    next();
-});
+
+
 
 // Method to compare passwords
 userSchema.methods.comparePassword = async function (enteredPassword) {
@@ -216,6 +210,7 @@ userSchema.methods.updateAverageRating = async function () {
     return result;
 };
 
+// Hash password before saving
 userSchema.pre("save", async function (next) {
     if (!this.isModified("password")) {
         return next();
