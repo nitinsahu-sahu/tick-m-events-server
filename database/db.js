e821448badf6a5d669a412bcc9f5efc230bb89fc
nitinsahu-sahu <nitinsahu911111@gmail.com>
2025-09-18 19:49:05 +0530

    Update Pannel

--- database/db.js
+++ database/db.js
@@ -15,11 +15,7 @@ const mongoose = require("mongoose");
 
 exports.connectToDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 5000 // Timeout after 5s if no server
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('✅ Database connected successfully.');
   } catch (error) {
     console.error('❌ MongoDB Error:', error.message);
