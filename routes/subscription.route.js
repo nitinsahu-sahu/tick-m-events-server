a5897b9ba12930de2619b35bbc82e530cef0a838
nitinsahu-sahu <nitinsahu911111@gmail.com>
2025-11-24 17:54:18 +0530

    Update placeAdb

--- routes/subscription.route.js
+++ routes/subscription.route.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { subscribe, unsubscribe } = require('../controllers/subscriptionController');
+const { subscribe } = require('../controllers/subscriptionController');
 const { validateSubscription } = require('../validators/event-validator');
 
-// POST /api/subscribe - Subscribe to newsletter
 router.post('/subscribe', validateSubscription, subscribe);
-
-// POST /api/unsubscribe - Unsubscribe from newsletter
-router.post('/unsubscribe', validateSubscription, unsubscribe);
 
 module.exports = router;
